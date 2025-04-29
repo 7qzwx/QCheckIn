e1b51e32ac02172d9afb47aba4cba0aa15584cd2
七种文学 <3117965903@qq.com>
2025-04-29 13:30:20 +0800

    修改主题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -26,6 +30,21 @@ android {
                 "proguard-rules.pro"
             )
         }
+        //自定义打包apk名称
+ applicationVariants.all {
+        val variant = this
+        variant.outputs.map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName =
+                    "QCheckIn${variant.versionName}_${
+                        SimpleDateFormat(
+                            "MMdd",
+                            Locale.getDefault()
+                        ).format(Date())
+                    }.apk"
+                output.outputFileName = outputFileName
+            }
+    }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -40,6 +59,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")  //修改系统状态栏
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/qzwx/app/qcheckin/ui/theme/Theme.kt
+++ app/src/main/java/qzwx/app/qcheckin/ui/theme/Theme.kt
@@ -1,57 +1,59 @@
 package qzwx.app.qcheckin.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xff00574A),
+    background = Color(0xFF141C25),
+    surface = Color(0xFF222A33),
+    onSurface = Color(0xFFF2F2F2),
 )
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
+    primary = Color(0xff48AB93),
+    background = Color(0xFFF2F2F2),
+    surface = Color(0xFFFFFFFF),
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
-
 @Composable
 fun QCheckInTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    darkTheme : Boolean = isSystemInDarkTheme(),
+    dynamicColor : Boolean = false,  // 是否根据主题变化颜色
+    content : @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme                                                      -> DarkColorScheme
+        else                                                           -> LightColorScheme
+    }
+    // 设置系统 状态栏颜色跟背景一致
+    val systemUiController = rememberSystemUiController()
+    val syscolor = if (isSystemInDarkTheme()) {
+       Color( 0xFF141C25)
+    } else {
+        Color(0xFFF2F2F2)
+    }
+    val darkiconscolor = !isSystemInDarkTheme()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = syscolor, // 状态栏
+            darkIcons = darkiconscolor, // 根据主题设置图标颜色
+        )
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        content = content
     )
 }
